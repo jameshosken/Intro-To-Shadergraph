9453848aecbec9abc75fec6703efb6cea6c629b8
James Hosken <jameswfhosken@gmail.com>
2020-04-02 15:00:41 -0500

    Add Workshop

---
+++ Shadergraph Workshop/Assets/DissolveHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DissolveHandler : MonoBehaviour
+{
+    [SerializeField] Material dissolveMaterial;
+
+    public void OnSliderChanged(float value)
+    {
+        dissolveMaterial.SetFloat("MixAmount", value);
+    }
+}
